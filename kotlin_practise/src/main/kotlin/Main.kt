c6dcf09465ac88d299540d1e4b5e283ada04303b
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-10-19 12:31:03 +0530

    added println code

--- kotlin_practise/src/main/kotlin/Main.kt
+++ kotlin_practise/src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 fun main(args: Array<String>) {
     println("Hello World!")
+    println("I am going to be billionaire in the future")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
