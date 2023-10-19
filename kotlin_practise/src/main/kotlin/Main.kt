24e0c8a368c3fc2bbd2f73bcbf935609f4ce78a2
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-10-19 12:42:25 +0530

    commented the old learning

--- kotlin_practise/src/main/kotlin/Main.kt
+++ kotlin_practise/src/main/kotlin/Main.kt
@@ -1,13 +1,13 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
-    println("I am going to be billionaire in the future")
-
-    var myVariable:Int = 10;
-
-    println("This is my variable: $myVariable")
-    myVariable =1;
-    println("The value is now $myVariable")
-
-    val constantVariable = 17;
-    println(constantVariable)
+//    println("Hello World!")
+//    println("I am going to be billionaire in the future")
+//
+//    var myVariable:Int = 10;
+//
+//    println("This is my variable: $myVariable")
+//    myVariable =1;
+//    println("The value is now $myVariable")
+//
+//    val constantVariable = 17;
+//    println(constantVariable)
 }
