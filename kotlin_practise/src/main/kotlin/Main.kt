6d3ae7423ec26ec8a3c95323ec815544aed87c15
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-10-19 12:34:21 +0530

    using variable and printing it

--- kotlin_practise/src/main/kotlin/Main.kt
+++ kotlin_practise/src/main/kotlin/Main.kt
@@ -2,4 +2,7 @@ fun main(args: Array<String>) {
     println("Hello World!")
     println("I am going to be billionaire in the future")
 
+    var myVariable:Int = 10;
+    println("This is my variable: $myVariable")
+
 }
